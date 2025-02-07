daa8af5fae227c944a1b2d328ae10dcb880123dd
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-02-07 17:06:43 -0600

    style: simplify more

--- tests/test_pets/test_pets.py
+++ tests/test_pets/test_pets.py
@@ -6,18 +6,7 @@ from tests.test_pets.base_test_pets import BaseTestPets
 def get_test_data(test_case):
     with open("data\\test_inputs\\pets\\test_data.json", 'r') as json_file:
         json_info = json.load(json_file).pop()
-    tmp_list = json_info.get(test_case)
-    params = []
-    # _tmp_tuple = []
-    for _tmp_dict in tmp_list:
-        _tmp_tuple = tuple(v for _, v in _tmp_dict.items())
-        # for _, v in _tmp_dict.items():
-        #     _tmp_tuple.append(v)
-        params.append(_tmp_tuple[::])
-        # _tmp_tuple.clear()
-    return params
-
-
+    return [tuple(v for _, v in _tmp_dict.items()) for _tmp_dict in json_info.get(test_case)]
 
 
 class TestPetsUploadImages(BaseTestPets):
