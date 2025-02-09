1b7f74a9857a5c8d3af394d0c5cd3908d87b60bd
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-02-08 23:42:54 -0600

    docs: docstrings

--- tests/test_pets/base_test_pets.py
+++ tests/test_pets/base_test_pets.py
@@ -1,9 +1,13 @@
+"""
+BaseTestPets class file and relates
+"""
 from src.pet_client import PetClient
 from tests.base_test import BaseTest
 
 
 class BaseTestPets(BaseTest):
     """
+    Base test class for Pets test classes
     """
     def setup(self, load_base_url, result):
         super().setup(load_base_url, PetClient, "pet", result)
